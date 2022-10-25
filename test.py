13b62c776afd1b8643493794d88ec3363763430b
joppimenta <joaopedropimenta@alu.ufc.br>
2022-10-25 20:13:49 -0300

    github test add

--- test.py
+++ test.py
@@ -21,4 +21,12 @@ name_donate = donate_button.get_attribute('name')
 print(name_donate)
 
 assert name_donate == 'submit'
+
+github_element = driver.find_element(By.XPATH, "/html/body/div/div[3]/div/a[3]")
+github_element.click()
+
+github_url = driver.current_url
+
+assert github_url == 'https://github.com/TestLinkOpenSourceTRMS/testlink-code/tree/testlink_1_9_20_fixed/'
+
 driver.quit()
