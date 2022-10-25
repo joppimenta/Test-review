e036be6d43215951ca56f8ba1ad61f9c3af45203
João Pedro Pimenta <86990174+joppimenta@users.noreply.github.com>
2022-10-25 13:58:57 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from behave import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from time import sleep
+import pyautogui
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://testlink.org/')
+title = driver.title
+assert title == 'TestLink'
+
+donate_button = driver.find_element(By.XPATH, "/html/body/div/div[3]/div/form/input[9]")
+print(donate_button)
+
+name_donate = donate_button.get_attribute('name')
+
+print(name_donate)
+
+assert name_donate == 'submit'
+driver.quit()
